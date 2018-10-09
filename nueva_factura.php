b14abccc2f613e759ddefc7b408e82ca1e6def04
jossafer96 <jossafer96@gmail.com>
2018-10-09 12:30:58 -0600

    Initial commit

---
+++ nueva_factura.php
@@ -0,0 +1,108 @@
+<?php
+	
+	$active_facturas="active";
+	$active_productos="";
+	$active_clientes="";
+	$active_usuarios="";	
+	$title="Nueva Factura | Tapices S. de R.L.";
+	
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include("cabecera.php");?>
+  </head>
+  <body>
+	<?php
+	include("barra_navegacion.php");
+	?>  
+    <div class="container">
+	<div class="panel panel-info">
+		<div class="panel-heading">
+			<h4><i class='glyphicon glyphicon-edit'></i> Nueva Factura</h4>
+		</div>
+		<div class="panel-body">
+		<?php 
+			include("modal/buscar_productos.php");
+			include("modal/registro_clientes.php");
+			include("modal/registro_productos.php");
+		?>
+			<form class="form-horizontal" role="form" id="datos_factura">
+				<div class="form-group row">
+				  <label for="nombre_cliente" class="col-md-1 control-label">Cliente</label>
+				  <div class="col-md-3">
+					  <input type="text" class="form-control input-sm" id="nombre_cliente" placeholder="Selecciona un cliente" required>
+					  <input id="id_cliente" type='hidden'>	
+				  </div>
+				  <label for="tel1" class="col-md-1 control-label">Teléfono</label>
+							<div class="col-md-2">
+								<input type="text" class="form-control input-sm" id="tel1" placeholder="Teléfono" readonly>
+							</div>
+					<label for="mail" class="col-md-1 control-label">Email</label>
+							<div class="col-md-3">
+								<input type="text" class="form-control input-sm" id="mail" placeholder="Email" readonly>
+							</div>
+				 </div>
+						<div class="form-group row">
+							<label for="empresa" class="col-md-1 control-label">Vendedor</label>
+							<div class="col-md-3">
+								<select class="form-control input-sm" id="id_vendedor">
+								</select>
+							</div>
+							<label for="tel2" class="col-md-1 control-label">Fecha</label>
+							<div class="col-md-2">
+								<input type="text" class="form-control input-sm" id="fecha" value="<?php echo date("d/m/Y");?>" readonly>
+							</div>
+							<label for="email" class="col-md-1 control-label">Pago</label>
+							<div class="col-md-3">
+								<select class='form-control input-sm' id="condiciones">
+									<option value="1">Efectivo</option>
+									<option value="2">Cheque</option>
+									<option value="3">Transferencia bancaria</option>
+									<option value="4">Crédito</option>
+								</select>
+							</div>
+						</div>
+				
+				
+				<div class="col-md-12">
+					<div class="pull-right">
+						<button type="button" class="btn btn-default" data-toggle="modal" data-target="#nuevoProducto">
+						 <span class="glyphicon glyphicon-plus"></span> Nuevo producto
+						</button>
+						<button type="button" class="btn btn-default" data-toggle="modal" data-target="#nuevoCliente">
+						 <span class="glyphicon glyphicon-user"></span> Nuevo cliente
+						</button>
+						<button type="button" class="btn btn-default" data-toggle="modal" data-target="#myModal">
+						 <span class="glyphicon glyphicon-search"></span> Agregar productos
+						</button>
+						<button type="submit" class="btn btn-default">
+						  <span class="glyphicon glyphicon-print"></span> Imprimir
+						</button>
+					</div>	
+				</div>
+			</form>	
+			
+		<div id="resultados" class='col-md-12' style="margin-top:10px"></div><!-- Carga los datos ajax -->			
+		</div>
+	</div>		
+		  <div class="row-fluid">
+			<div class="col-md-12">
+			
+	
+
+			
+			</div>	
+		 </div>
+	</div>
+	<hr>
+	<?php
+	include("pie_pagina.php");
+	?>
+	<script type="text/javascript" src="js/VentanaCentrada.js"></script>
+	<script type="text/javascript" src="js/nueva_factura.js"></script>
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+
+  </body>
+</html>
---
+++ productos.php
@@ -0,0 +1,72 @@
+<?php
+	
+	
+	$active_facturas="";
+	$active_productos="active";
+	$active_clientes="";
+	$active_usuarios="";	
+	$title="Productos | Tapices S. de R.L.";
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php include("cabecera.php");?>
+  </head>
+  <body>
+	<?php
+	include("barra_navegacion.php");
+	?>
+	
+    <div class="container">
+	<div class="panel panel-info">
+		<div class="panel-heading">
+		    <div class="btn-group pull-right">
+				<button type='button' class="btn btn-info" data-toggle="modal" data-target="#nuevoProducto"><span class="glyphicon glyphicon-plus" ></span> Nuevo Producto</button>
+			</div>
+			<h4><i class='glyphicon glyphicon-search'></i> Buscar Productos</h4>
+		</div>
+		<div class="panel-body">
+		
+			
+			
+			<?php
+			include("modal/registro_productos.php");
+			include("modal/editar_productos.php");
+			?>
+			<form class="form-horizontal" role="form" id="datos_cotizacion">
+				
+						<div class="form-group row">
+							<label for="q" class="col-md-2 control-label">Código o nombre</label>
+							<div class="col-md-5">
+								<input type="text" class="form-control" id="q" placeholder="Código o nombre del producto" onkeyup='load(1);'>
+							</div>
+							<div class="col-md-3">
+								<button type="button" class="btn btn-default" onclick='load(1);'>
+									<span class="glyphicon glyphicon-search" ></span> Buscar</button>
+								<span id="loader"></span>
+							</div>
+							
+						</div>
+				
+				
+				
+			</form>
+				<div id="resultados"></div><!-- Carga los datos ajax -->
+				<div class='outer_div'></div><!-- Carga los datos ajax -->
+			
+		
+	
+			
+			
+			
+  </div>
+</div>
+		 
+	</div>
+	<hr>
+	<?php
+	include("pie_pagina.php");
+	?>
+	<script type="text/javascript" src="js/productos.js"></script>
+  </body>
+</html>
